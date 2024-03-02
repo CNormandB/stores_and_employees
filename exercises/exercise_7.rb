76d1163b626110c33b4b09728a683a6b991f905d
Casandra Normand <casandra.normand@outlook.com>
2024-03-01 22:23:52 -0500

    feat: Add validations to two models to enforce the business rules

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -2,7 +2,7 @@
 #1. Add validations to two models to enforce the following business rules:
   #- Employees must always have a first name present
   #- Employees must always have a last name present
-  #-  Employees have a hourly_rate that is a number (integer) between 40 and 200
+  #- Employees have a hourly_rate that is a number (integer) between 40 and 200
   #- Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
   #- Stores must always have a name that is a minimum of 3 characters
   #- Stores have an annual_revenue that is a number (integer) that must be 0 or more
@@ -20,6 +20,39 @@ require_relative './exercise_5'
 require_relative './exercise_6'
 
 puts "Exercise 7"
-puts "----------"
 
-# Your code goes here ...
+#Add validations to two models to enforce the business rules (stated above)
+class Store < ActiveRecord::Base
+  has_many :employees
+#Stores must always have a name that is a minimum of 3 characters
+  validates :name, length: { minimum: 3 }
+#Stores have an annual_revenue that is a number (integer) that must be 0 or more
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+#Employees must always have a first name present
+  validates :first_name, presence: true
+#Employees must always have a last name present
+  validates :last_name, presence: true
+#Employees have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+#Employees must always have a store that they belong to
+  validates_associated :store
+end
+
+# Ask the user for a store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name, leaving out other fields
+new_store = Store.new(name: store_name)
+
+# Display error messages if any
+if !new_store.valid?
+  puts "Errors occurred while creating the store:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
